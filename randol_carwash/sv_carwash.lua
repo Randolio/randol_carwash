803e04bfbdbade118f2b7b7f6b89be8d12e54ea5
Randolio <103154494+Randolio@users.noreply.github.com>
2023-03-16 09:11:35 +0000

    braindead lul.

--- randol_carwash/sv_carwash.lua
+++ randol_carwash/sv_carwash.lua
@@ -1,20 +1,13 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent("randol_carwash:server:removeCost", function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local cost = Config.Cost
-    Player.Functions.RemoveMoney('bank', cost)
-    QBCore.Functions.Notify(src, 'You paid $'..cost..' to get a car wash.', 'success')
-    TriggerClientEvent('randol_carwash:client:startWash', src)
-end)
-
 QBCore.Functions.CreateCallback('randol_carwash:server:canAfford', function(source, cb)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local bankBalance = Player.PlayerData.money.bank
     local cost = Config.Cost
     if bankBalance >= cost then
+        Player.Functions.RemoveMoney('bank', cost)
+        QBCore.Functions.Notify(src, 'You paid $'..cost..' to get a car wash.', 'success')
         cb(true)
     else
         QBCore.Functions.Notify(src, 'You need $'..cost..' in your bank to get a car wash.', 'error')
