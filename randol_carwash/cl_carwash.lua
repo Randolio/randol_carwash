983620b934b1fd87961faca6581300de167f2ac6
Randolio <103154494+Randolio@users.noreply.github.com>
2023-11-24 08:35:22 +0000

    Update cl_carwash.lua

--- randol_carwash/cl_carwash.lua
+++ randol_carwash/cl_carwash.lua
@@ -1,90 +1,66 @@
-local QBCore = exports['qb-core']:GetCoreObject()
 local washingVehicle = false
-local washRadial = nil
 
-local function setRadial()
-    if not washRadial then
-        washRadial = exports['qb-radialmenu']:AddOption({
-            id = 'car_wash',
-            title = 'Wash Vehicle',
-            icon = 'car',
-            type = 'client',
-            event = 'randol_carwash:client:startWash',
-            shouldClose = true
-        })
+local function washCar()
+    if washingVehicle then return end
+    washingVehicle = true
+    local result = lib.callback.await("randol_carwash:server:canAfford", false)
+    if result then
+        local vehicle = GetVehiclePedIsIn(cache.ped)
+        QBCore.Functions.Progressbar("wash_car", "Washing vehicle..", Config.WashTime, false, false, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+        }, {}, {}, {}, function() -- Done
+            SetVehicleDirtLevel(vehicle, 0.0)
+            WashDecalsFromVehicle(vehicle, 1.0)
+            washingVehicle = false
+            QBCore.Functions.Notify('Your car is now clean.', 'success')
+        end)
+    else
+        washingVehicle = false
     end
 end
 
-CreateThread(function()
-    local carwashZones = {}
-
-    for k, v in pairs(Config.CarWashLocs) do
-        carwashZones[#carwashZones + 1] = PolyZone:Create(v.PolyZone, {
-            name = k,
-            minZ = v.minZ,
-            maxZ = v.maxZ,
-            debugPoly = false
-        })
+local function onEnter(self)
+    if IsPedInAnyVehicle(cache.ped, false) then
+	    lib.showTextUI('[E] - Wash Car', { icon = 'fa-solid fa-car', position = 'left-center', })
     end
+end
+ 
+local function onExit(self)
+	lib.hideTextUI()
+end
 
-    local washCombo = ComboZone:Create(carwashZones, {
-        name = "washCombo",
-        debugPoly = false
+local function inside(self)
+    if IsControlJustReleased(0, 38) then
+        local inVehicle = IsPedInAnyVehicle(cache.ped, false)
+        local currentVeh = GetVehiclePedIsIn(cache.ped, false)
+        local driver = GetPedInVehicleSeat(currentVeh, -1)
+        if inVehicle and not washingVehicle then 
+            if driver == cache.ped then
+                washCar()
+            end
+        end
+    end
+end
+
+for i = 1, #Config.CarWashLocs do
+    local zone = lib.zones.poly({
+        points = Config.CarWashLocs[i].points,
+        thickness = 9.0,
+        debug = false,
+        onEnter = onEnter,
+        inside = inside,
+        onExit = onExit,
     })
-    
-    washCombo:onPlayerInOut(function(isPointInside)
-        if isPointInside then
-            local inVehicle = IsPedInAnyVehicle(PlayerPedId(), false)
-            local currentVeh = GetVehiclePedIsIn(PlayerPedId(), false)
-            local driver = GetPedInVehicleSeat(currentVeh, -1)
-            if inVehicle and not washRadial then 
-                if driver == PlayerPedId() then
-                    exports['qb-core']:DrawText('Car Wash $'..Config.Cost, 'left')
-                    setRadial()
-                end
-            end
-        else
-            if washRadial then
-                exports['qb-radialmenu']:RemoveOption(washRadial)
-                washRadial = nil
-                exports['qb-core']:HideText()
-            end
-        end
-    end)
-end)
-
-RegisterNetEvent('randol_carwash:client:startWash', function()
-    if washingVehicle then return end
-    QBCore.Functions.TriggerCallback("randol_carwash:server:canAfford",function(result)
-        if result then
-            local vehicle = GetVehiclePedIsIn(PlayerPedId())
-            washingVehicle = true
-            QBCore.Functions.Progressbar("wash_car", "Washing vehicle..", Config.WashTime, false, false, {
-                disableMovement = true,
-                disableCarMovement = true,
-                disableMouse = false,
-                disableCombat = true,
-            }, {}, {}, {}, function() -- Done
-                SetVehicleDirtLevel(vehicle, 0.0)
-                WashDecalsFromVehicle(vehicle, 1.0)
-                washingVehicle = false
-                QBCore.Functions.Notify('Your car is now clean.', 'success')
-            end)
-        end
-    end)
-end)
-
-CreateThread(function()
-    for location in pairs(Config.CarWashLocs) do
-        local loc = Config.CarWashLocs[location].Blip
-        local washBlip = AddBlipForCoord(loc.x, loc.y, loc.z)
-        SetBlipSprite(washBlip, 100)
-        SetBlipDisplay(washBlip, 4)
-        SetBlipScale(washBlip, 0.75)
-        SetBlipAsShortRange(washBlip, true)
-        SetBlipColour(washBlip, 37)
-        BeginTextCommandSetBlipName("STRING")
-        AddTextComponentSubstringPlayerName(Config.CarWashLocs[location].Label)
-        EndTextCommandSetBlipName(washBlip)
-    end
-end)
+    local washBlip = AddBlipForCoord(Config.CarWashLocs[i].Blip.x, Config.CarWashLocs[i].Blip.y, Config.CarWashLocs[i].Blip.z)
+    SetBlipSprite(washBlip, 100)
+    SetBlipDisplay(washBlip, 4)
+    SetBlipScale(washBlip, 0.75)
+    SetBlipAsShortRange(washBlip, true)
+    SetBlipColour(washBlip, 37)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(Config.CarWashLocs[i].Label)
+    EndTextCommandSetBlipName(washBlip)
+end
