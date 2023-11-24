5d061a31d2d495ab1691bc56d83f8d52f5de0285
Randolio <103154494+Randolio@users.noreply.github.com>
2023-11-24 08:36:35 +0000

    Update fxmanifest.lua

--- randol_carwash/fxmanifest.lua
+++ randol_carwash/fxmanifest.lua
@@ -4,20 +4,10 @@ game 'gta5'
 author 'Randolio'
 description 'Car Wash Zones'
 
-shared_scripts {
-	'config.lua'
-}
+shared_scripts {'config.lua', '@ox_lib/init.lua'}
 
-server_scripts {
-    'sv_carwash.lua'
-}
+server_scripts {'sv_carwash.lua'}
 
-client_scripts {
-	'cl_carwash.lua',
-	'@PolyZone/client.lua',
-    '@PolyZone/CircleZone.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/ComboZone.lua',
-}
+client_scripts {'cl_carwash.lua'}
 
 lua54 'yes'
