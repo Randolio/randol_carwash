32f1b25d20da084e652856ae3c6dd165172db431
Randolio <103154494+Randolio@users.noreply.github.com>
2023-11-24 08:35:56 +0000

    Update sv_carwash.lua

--- randol_carwash/sv_carwash.lua
+++ randol_carwash/sv_carwash.lua
@@ -1,6 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-QBCore.Functions.CreateCallback('randol_carwash:server:canAfford', function(source, cb)
+lib.callback.register('randol_carwash:server:canAfford', function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local bankBalance = Player.PlayerData.money.bank
@@ -8,9 +6,9 @@ QBCore.Functions.CreateCallback('randol_carwash:server:canAfford', function(sour
     if bankBalance >= cost then
         Player.Functions.RemoveMoney('bank', cost)
         QBCore.Functions.Notify(src, 'You paid $'..cost..' to get a car wash.', 'success')
-        cb(true)
+        return true
     else
         QBCore.Functions.Notify(src, 'You need $'..cost..' in your bank to get a car wash.', 'error')
-        cb(false)
+        return false
     end
 end)
